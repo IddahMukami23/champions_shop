d677fd21615840b9d53ce24bce5cf12b7bf22970
IddahMukami23 <miritikathini@gmail.com>
2023-11-13 14:43:58 +0300

    mpesa daraja payment api intergrated but yet to be dedugged

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
-    path('add-products/', views.add_products,name='add-products'),
+    path('add-products/', views.add_products, name='add-products'),
+    path('delete/<id>', views.delete, name='delete'),
+    path('update/<id>', views.update_products, name='delete'),
+    path('payments/<id>', views.pay, name='pay'),
 ]
